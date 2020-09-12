3f7272d62c68e45e5c83064d6c889e8934fb8f9e
mdakhib <mohamedakhib@gmail.com>
2020-09-12 13:18:23 +0530

    post function API added-Completed

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express=require("express");
 const https=require("https");
+const bodyparser=require("body-parser");
 
 const app=express();
 
+app.use(bodyparser.urlencoded({extended:true}));
+
 app.get("/",(req,res)=>{
-    const url="https://api.openweathermap.org/data/2.5/weather?q=India&appid=1639dcd7a98bb7a769101c657a172472&units=metric&q=Mysore"
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/",(req,res)=>{
+    const query=req.body.cityName
+    const apiKey="1639dcd7a98bb7a769101c657a172472"
+    const unit="metric"
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units="+ unit +"&q=Mysore"
     https.get(url,function(response){
         console.log(response.statusCode);
         response.on("data",function(data){
@@ -12,9 +22,9 @@ app.get("/",(req,res)=>{
             const temp= weatherData.main.feels_like;
             const weatherDescription=weatherData.weather[0].description;
             const icon=weatherData.weather[0].icon
-            const imageURL= "http://openweathermap.org/img/wn/"+icon+"@2x.png"
+            const imageURL= "http://openweathermap.org/img/wn/"+ icon +"@2x.png"
             
-            res.write(`<h1>The Temperature in India is ${temp} degrees celcius.</h1>`);
+            res.write(`<h1>The temperature in ${query} is ${temp} degrees celcius.</h1>`);
             res.write(`<h3>The weather is currently ${weatherDescription}</h3>`);
             res.write("<img src=" + imageURL +">");
             res.send();
@@ -22,7 +32,6 @@ app.get("/",(req,res)=>{
         })
     });
 })
-
 
 
 app.listen(3000,()=>{
